3d2d4e2c116444dddff6d6be9909e36f995c1915
Natasha Gallagher <nbhat1@gmail.com>
2020-07-18 07:02:03 +0000

    Added functions for add_mult and add_cubes

--- arithmetic.py
+++ arithmetic.py
@@ -39,3 +39,14 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1 % num2
+
+def add_mult (num1, num2, num3):
+    """ Return the sum of num1 and num 2 multiplied by num3"""
+
+    return multiply(add(num1,num2), num3)
+
+
+def add_cubes (num1, num2):
+    """Return the sum of the cube of both numbers"""
+
+    return cube(num1) + cube(num2)
